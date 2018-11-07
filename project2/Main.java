22369b1dcfe255e913a1a198f7fb92c88f6a6d45
tlind15 <tlind15@gmail.com>
2018-11-06 20:15:35 -0800

    Add project 2 folder

---
+++ project2/Main.java
@@ -0,0 +1,20 @@
+
+public class Main 
+{
+	public static void main(String[] args)
+	{
+		Person p1 = new Person();
+		Person p2 = new Person();
+	
+		Name name = new Name("Mr", "John", "Henry", "Doe");
+		p2.setName(name);
+		
+		System.out.println(p1.getName());
+		System.out.println(p1.getFirstName());
+		System.out.println(p1.getLastName());
+		
+		System.out.println(p2.getName());
+		System.out.println(p2.getFirstName());
+		System.out.println(p2.getLastName());
+	}
+}
